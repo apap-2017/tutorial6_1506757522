4a590547f1a03c8087e010f5d3bd1bcefc52bdb2
DESKTOP-8H3JI9I\Hp <joachimichwan@gmail.com>
2017-10-19 21:36:47 +0700

    tutorial 6

---
+++ src/main/java/com/example/service/StudentService.java
@@ -0,0 +1,21 @@
+package com.example.service;
+
+import java.util.List;
+
+import com.example.model.CourseModel;
+import com.example.model.StudentModel;
+
+public interface StudentService
+{
+    StudentModel selectStudent (String npm);
+
+    List<StudentModel> selectAllStudents ();
+
+    void addStudent (StudentModel student);
+
+    void updateStudent (StudentModel student);
+    
+    void deleteStudent (String npm);
+
+	CourseModel selectCourse(String id_course);
+}
